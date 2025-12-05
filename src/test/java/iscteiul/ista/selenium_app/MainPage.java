4fbc8bf8c02241680ec9630f002a0b72a1e978c2
54348 <72211947+LETI-54348@users.noreply.github.com>
2025-12-05 21:49:54 +0000

    Update MainPage.java

--- src/test/java/iscteiul/ista/selenium_app/MainPage.java
+++ src/test/java/iscteiul/ista/selenium_app/MainPage.java
@@ -8,27 +8,22 @@ public class MainPage {
 
     // --- TOP HEADER ---
 
-    // Botão Developer Tools (abre o submenu)
     public SelenideElement toolsMenu =
             $("button[data-test='main-menu-item-action'][aria-label*='Developer Tools']");
 
-    // --- SUBMENU (menu que aparece após clicar Developer Tools) ---
+    // --- SUBMENU ---
 
-    // "See All Developer Tools"
     public SelenideElement seeDeveloperToolsButton =
             $("a[data-test='see-developer-tools']");
 
-    // "Find your tools"
     public SelenideElement findYourToolsButton =
             $("a[data-test='products-find-your-tools']");
 
     // --- SEARCH ---
 
-    // Botão do ícone da lupa
     public SelenideElement searchIcon =
             $("[data-test='site-header-search-action']");
 
-    // Campo de input do popup de pesquisa
     public SelenideElement searchInput =
             $("[data-test-id='search-input']");
 }
